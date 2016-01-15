7f91209483aff7fdbfc8d1643c906aa92666e892
mininao <maxence_aici@yahoo.fr>
2016-01-15 15:51:34 +0100

    Adding sample data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+var app = new Vue({
+	el: '#app',
+	data: {
+		cities: {}
+	}
+})
+
+var ref = new Firebase("https://publicdata-weather.firebaseio.com");
+
+ref.on("value",function(snap){
+	app.cities = snap.val()
+});
